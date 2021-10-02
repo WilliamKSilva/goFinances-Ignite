c103cde833a3189f48bb5767dc9bbb68ad8b2528
vinimedeiros13 <vinimedeiros7@outlook.com>
2021-10-02 11:22:49 -0300

    adding dark mode

--- App.tsx
+++ App.tsx
@@ -1,35 +1,47 @@
 import { StatusBar } from 'expo-status-bar';
 import AppLoading from 'expo-app-loading';
-import { ThemeProvider } from 'styled-components';  
+import { ThemeProvider } from 'styled-components';
+
+import { useState } from 'react'
 
 import {
-  useFonts, 
-  Poppins_400Regular, 
-  Poppins_500Medium, 
+  useFonts,
+  Poppins_400Regular,
+  Poppins_500Medium,
   Poppins_700Bold,
 } from '@expo-google-fonts/poppins';
 
 import React from 'react';
 import { Dashboard } from './src/screens/dashboard';
 
-import theme from './src/global/styles/theme';
+import { light } from './src/global/styles/light';
+import { dark } from './src/global/styles/dark';
+import { Button } from 'react-native';
 
-export default function App() { 
+
+export default function App() {
+  const [theme, setTheme] = useState(light);
+
+  const toggleTheme = () => {
+    setTheme(theme === light ? dark : light);
+  }
+
   const [fontLoaded] = useFonts({
-    Poppins_400Regular, 
-    Poppins_500Medium, 
+    Poppins_400Regular,
+    Poppins_500Medium,
     Poppins_700Bold
 
   });
 
-  if(!fontLoaded){
+  if (!fontLoaded) {
     return <AppLoading />
   }
   return (
     <ThemeProvider theme={theme}>
-        <Dashboard />
+      <Dashboard />
+      <Button onPress={toggleTheme} title={"BOTAO"}/>
     </ThemeProvider>
-        
+
   );
 }
 
