fa4fd8aa37171231cad2e30565ac4ec33d74a9cf
Williamkds <wilz.kelvin12@gmail.com>
2021-10-04 10:13:03 -0300

    Register Screens

--- App.tsx
+++ App.tsx
@@ -10,7 +10,7 @@ import {
 } from '@expo-google-fonts/poppins';
 
 import React from 'react';
-import { Dashboard } from './src/screens/dashboard';
+import { Register } from './src/screens/Register';
 
 import theme from './src/global/styles/theme';
 
@@ -27,7 +27,7 @@ export default function App() {
   }
   return (
     <ThemeProvider theme={theme}>
-        <Dashboard />
+        <Register />
     </ThemeProvider>
         
   );
