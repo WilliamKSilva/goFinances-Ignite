cfc97505e6e785f0ba33b5c4b9be23c7587a6a0f
Williamkds <wilz.kelvin12@gmail.com>
2021-10-11 02:40:59 -0300

    Now the user can save multiple transactions without them overwrite each other

--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -71,7 +71,7 @@ export function Register() {
         if(category.key === 'category')
           return Alert.alert("Selecione a categoria");
 
-        const data = {
+        const newTransaction = {
             name: form.name,
             amount: form.amount,
             transactionType, 
@@ -80,7 +80,16 @@ export function Register() {
         
         try {
 
-          await AsyncStorage.setItem(dataKey, JSON.stringify(data));
+          const data = await AsyncStorage.getItem(dataKey);
+          const currentData = data ? JSON.parse(data) : [];
+
+          const dataFormatted = [
+              ...currentData, 
+              newTransaction
+
+          ];
+
+          await AsyncStorage.setItem(dataKey, JSON.stringify(dataFormatted));
 
         } catch (error) {
             console.log(error);
@@ -90,11 +99,11 @@ export function Register() {
 
     useEffect(() => {
         async function loadData() {
-           const data = await AsyncStorage.getItem(dataKey);
-           console.log(JSON.parse(data!));
-        } 
-
-        loadData();
+            const data = await AsyncStorage.getItem(dataKey);
+            console.log(JSON.parse(data!));
+         } 
+ 
+         loadData();
     },[])
 
     return (
